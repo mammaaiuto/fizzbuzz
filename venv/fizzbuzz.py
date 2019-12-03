39f25f6913eaf11d60e34862902b16b1daae02ff
yuutarou <mammaaiuto0522@gmail.com>
2019-12-03 18:05:02 +0900

    FizzBuzz変換用の関数の再デザイン
    convertだけではなく出力も担当していたのでconvert専属にした

--- venv/fizzbuzz.py
+++ venv/fizzbuzz.py
@@ -2,20 +2,13 @@
 
 def fizzbuzz_convert(number):
     if number % 15 == 0:
-        print("FizzBuzz")
+        return "FizzBuzz"
     elif number % 3 == 0:
-        print("Fizz")
+        return "Fizz"
     elif number % 5 == 0:
-        print("Buzz")
+        return "Buzz"
     else:
-        print(str(number))
+        return str(number)
 
-fizzbuzz_convert(1)
-fizzbuzz_convert(3)
-fizzbuzz_convert(34)
-fizzbuzz_convert(67)
-fizzbuzz_convert(8)
-fizzbuzz_convert(68)
-fizzbuzz_convert(90)
-fizzbuzz_convert(100)
-fizzbuzz_convert(34)
+result = fizzbuzz_convert(1)
+print(result)
